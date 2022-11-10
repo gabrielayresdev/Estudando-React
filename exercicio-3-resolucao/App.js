5808d28d587c49ed5c22b64682f276222ac24eca
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-09 22:12:51 -0300

    Resolução do exercício 3

---
+++ exercicio-3-resolucao/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
+const produtos = [
+  { nome: 'Notebook', propriedades: ['16gb ram', '512gb'] },
+  { nome: 'Smartphone', propriedades: ['2gb ram', '128gb'] },
+];
+
+const App = () => {};
+
+export default App;
+//rafce
---
+++ exercicio-3-resolucao/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/Produtos">Produtos</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
