3f1bdbc8ef87209d0c7018c4d20471e73eeb2611
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-10 22:59:49 -0300

    Create exercicio-4.js

---
+++ exercicio-4.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
+//rafce
