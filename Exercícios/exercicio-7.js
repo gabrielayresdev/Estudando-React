2352ec2423a5bce9786a05fd6bdc6ab1eeb28ea6
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-14 23:58:46 -0300

    Create exercicio-7.js

---
+++ Exercícios/exercicio-7.js
@@ -0,0 +1,23 @@
+import React from `react`;
+
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+// Essa é a função utilizado para realizar o POST
+fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // form é o objeto com os dados do formulário
+  body: JSON.stringify(form),
+});
+
+// Mostre uma mensagem na tela, caso a resposta da API seja positiva
+
+const App = () => {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
