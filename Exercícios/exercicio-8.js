194c34f50159b609667acc61406ab22e533e98c7
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-15 18:38:51 -0300

    Create exercicio-8.js

---
+++ Exercícios/exercicio-8.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+const App = () => {
+
+  return (
+    <h1>
+      Hello World
+    </h1>
+  );
+};
+
+export default App;
+//rafce
