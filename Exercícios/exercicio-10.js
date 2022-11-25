0a91f66d8a30ed76dbca142808c7d0d77055b9db
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-24 23:46:22 -0300

    Create exercicio-10.js

---
+++ Exercícios/exercicio-10.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Utilize a API abaixo para puxar a lista de produto
+// https://ranekapi.origamid.dev/json/api/produto
+// Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+
+const App = () => {
+  return <></>;
+}
+
+export default App;
